c717b3fc3ab33f1d5b172aa9489842c896d54438
Jhossed Molina <jhossedmolina@gmail.com>
2022-04-19 19:19:42 -0500

    Creacion del SideNav de la aplicacion

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnDestroy {
+
+  mobileQuery: MediaQueryList;
+
+  title = "REPUBLICA DE LOS COCOS";
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+
+}
+
